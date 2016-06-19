97b8a02e1fc1a8137ee1b449ac93b706a6bf41a1
ksheng <294851575@qq.com>
2016-06-19 16:57:41 +0800

    完成了定时提醒 notification的使用 使用了ScheduledExecutorService线程池保证每60秒确认一次

--- app/src/main/java/service/ClockService.java
+++ app/src/main/java/service/ClockService.java
@@ -2,6 +2,7 @@ package service;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.database.Cursor;
@@ -17,8 +18,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import Utils.MySQLHelper;
+import activity.HomeActivity;
 import skkk.admin.com.dakai_station.R;
 
 public class ClockService extends Service {
@@ -49,51 +54,58 @@ public class ClockService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("AAAAAAAAA", "onCreate");
-        Toast.makeText(ClockService.this, "aaa", Toast.LENGTH_SHORT).show();
+        Log.d("AAAAAAAAA", "提醒服务已开启");
+        Toast.makeText(ClockService.this, "提醒服务已开启", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         nowDate = System.currentTimeMillis();
         mySQLHelper = new MySQLHelper(this,"clock.db",null,1);
-        db = mySQLHelper.getReadableDatabase();
-        cursor = db.rawQuery("select train_id,time,start,end,from_time,to_time,item_1,item_2 from Clock", null);
-        dataList = new ArrayList<>();
-        while (cursor.moveToNext()){
 
-            data = new ArrayList<>();
-            data.add(cursor.getString(0));//train_id
-            data.add(cursor.getString(2));//start
-            data.add(cursor.getString(3));//end
-            data.add(cursor.getString(4));//start_time
-            data.add(cursor.getString(5));//end_time
-            data.add(String.valueOf(cursor.getPosition()));
+        ScheduledExecutorService scheduledThreadPool= Executors.newScheduledThreadPool(1);
+        scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                db = mySQLHelper.getReadableDatabase();
+                cursor = db.rawQuery("select train_id,time,start,end,from_time,to_time,item_1,item_2 from Clock", null);
+                dataList = new ArrayList<>();
+                while (cursor.moveToNext()){
 
-            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟
-            try {
-                aaa = cursor.getString(1)+" "+cursor.getString(4)+":00";
-                timeDate = sdf.parse(aaa);
-            } catch (ParseException e) {
-                e.printStackTrace();
+                    data = new ArrayList<>();
+                    data.add(cursor.getString(0));//train_id
+                    data.add(cursor.getString(2));//start
+                    data.add(cursor.getString(3));//end
+                    data.add(cursor.getString(4));//start_time
+                    data.add(cursor.getString(5));//end_time
+                    data.add(String.valueOf(cursor.getPosition()));
+
+                    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//小写的mm表示的是分钟
+                    try {
+                        aaa = cursor.getString(1)+" "+cursor.getString(4)+":00";
+                        timeDate = sdf.parse(aaa);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    clockDate_24_long=timeDate.getTime()-diff_24;
+                    clockDate_6_long=timeDate.getTime()-diff_6;
+                    clockDate_2_long=timeDate.getTime()-diff_2;
+
+                    notificationDeal(clockDate_24_long,24);
+                    notificationDeal(clockDate_6_long,6);
+                    notificationDeal(clockDate_2_long,2);
+
+                    dataList.add(data);
+                }
+                if(cursor!=null) {
+                    cursor.close();
+                }
+                if(db!=null){
+                    db.close();
+                }
             }
-
-            clockDate_24_long=timeDate.getTime()-diff_24;
-            clockDate_6_long=timeDate.getTime()-diff_6;
-            clockDate_2_long=timeDate.getTime()-diff_2;
-
-            notificationDeal(clockDate_24_long,24);
-            notificationDeal(clockDate_6_long,6);
-            notificationDeal(clockDate_2_long,2);
-
-            dataList.add(data);
-        }
-        if(cursor!=null) {
-            cursor.close();
-        }
-        if(db!=null){
-            db.close();
-        }
+        },1,60, TimeUnit.SECONDS);
 
         Log.d("AAAAAAAAA", "Start");
         return super.onStartCommand(intent, flags, startId);
@@ -101,21 +113,28 @@ public class ClockService extends Service {
     }
 
     private void notificationDeal(long clockTime,int hours) {
-        if (clockTime<nowDate){
+
+        if (nowDate-clockTime>=0&&nowDate-clockTime<60000){
             manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
             builder = new NotificationCompat.Builder(this);
+            Intent intent=new Intent(this, HomeActivity.class);
+            PendingIntent pi=PendingIntent.getActivity(this,0,intent,0);
             notification = builder
-                    .setContentTitle("提醒")
-                    .setContentText("从 "+data.get(1)+" 开往 "+data.get(2)+" 的列车 "+data.get(0)+" 还有"+hours+"小时就要出发了")
+                    .setAutoCancel(true)//点击之后自动消失
+                    .setContentTitle("提醒")//标题内容
+                    .setContentText("从 "+data.get(1)+" 开往 "+data.get(2)+
+                            " 的列车 "+data.get(0)+" 还有"+hours+"小时就要出发了")//文本内容
                     .setTicker("提前"+hours+"小时火车提醒")
-                    .setDefaults(Notification.DEFAULT_VIBRATE)
-                    .setWhen(System.currentTimeMillis())
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                    .setDefaults(Notification.DEFAULT_VIBRATE)//默认的闪光灯震动提醒
+                    .setWhen(System.currentTimeMillis())//通知时间
+                    .setSmallIcon(R.mipmap.ic_launcher)//设置通知小图标
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                            R.mipmap.ic_launcher))//设置通知的大图标
+                    .setContentIntent(pi)
                     .build();
             manager.notify(Integer.valueOf(data.get(5)), notification);
-
-
+        }else{
+            Log.d("CCCCCCCCCCCCCCCCCCC","执行无果，完毕");
         }
     }
 
